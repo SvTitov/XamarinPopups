b0dbffbf8316d3babb37a3051800cf81feb5fe81
Svyatoslav Titov <slava.titoff@yandex.ru>
2018-04-13 15:46:54 +0400

    init

---
+++ Droid.Popup/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace Droid.Popup
+{
+    [Activity(Label = "Droid.Popup", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        DrawerLayout _drawerLayout;
+        Android.Support.V7.Widget.Toolbar _toolbar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            _toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.tool_bar);
+            SetSupportActionBar(_toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+            SupportActionBar.Title = "Term and Condition";
+
+            _drawerLayout = FindViewById<DrawerLayout>(Resource.Id.MainActivity_DrawerLayout);
+            FragmentManager.BeginTransaction()
+                .Replace(Resource.Id.MainActivity_ContentFrame, new PopupFragment())
+                .Commit();
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    OnBackPressed();
+                    return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
---
+++ Droid.Popup/PopupFragment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+
+namespace Droid.Popup
+{
+    public class PopupFragment : Fragment
+    {
+        WebView _webView;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+
+            _webView.LoadUrl($@"file:///android_asset/hh.html");
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            
+            var view = inflater.Inflate(Resource.Layout.fg_Popup, container, false);
+            _webView = view.FindViewById<WebView>(Resource.Id.webview);
+
+            return view;
+        }
+    }
+}
