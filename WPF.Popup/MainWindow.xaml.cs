2ec790e43d9f2d354200faa06087ab16c7260614
Svyatoslav Titov <slava.titoff@yandex.ru>
2018-04-17 12:41:09 +0400

    Added wpf

---
+++ WPF.Popup/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+
+namespace WPF.Popup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"hh.html");
+            this.webBrowser.Source = new Uri(path);
+        }
+    }
+}
